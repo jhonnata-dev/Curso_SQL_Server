3945f900f647cf83618d262a83eb5ad2f6587209
Jhonnata-dev <jhonnatadmartins@gmail.com>
2025-03-10 00:18:26 -0300

    Conceito de NULL

---
+++ Seção 07 - Como consultar dados/05 - Conceito de NULL/conceito-de-null.sql
@@ -0,0 +1,12 @@
+/*
+NULL
+
+Em SQL Server, o valor NULL indica uma indisponibilidade, ou seja, algo indisponível ou não atribuído.
+
+Importante ressaltar que NULL não é igual a zero e nem igual a um espaço (' ') porque ambos são valores atribuídos e NULL representa o contrário,
+uma não atribuição de valor. Como o NULL não pode ser igual ou diferente de qualquer valor, usamos (1)IS NULL ou (2)IS NOT NULL que em outras palavras
+são: (1)tem valor atribuído/disponível ou (2)não tem valor atribuído/disponível, por isso é importante realizar os filtros comparando IS NULL ou IS NOT
+NULL e os outros demais valores como por exemplo: zero ou espaço para ter maior assertividade no retorno de sua consulta.
+
+Na próxima aula vamos aprender da prática como utilizar o NULL.
+*/
